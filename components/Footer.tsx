3cd20864ad2ce7d26e8b1182e580662d67ac097a
Hijir Ismail <hijir512@gmail.com>
2025-07-16 12:44:11 +0800

    feat: add Footer components

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Logo from "@/public/logo.webp";
+
+export default function Footer() {
+    return (
+        <footer className="bg-blue-300 text-black p-4 text-center mt-10 flex items-center gap-0.5 md:gap-3 justify-center">
+            <Image src={Logo} alt="Logo" width={40} height={40}></Image>
+            <p className="text-sm ">&copy; 2025 Pantai Galesong | Develop by GercepTechnology</p>
+        </footer>
+    );
+}
